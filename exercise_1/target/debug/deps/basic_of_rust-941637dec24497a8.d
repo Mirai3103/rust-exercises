d1025766b95cae5596a09595756d0fba76de8478
Mirai3103 <huuhoag1412@gmail.com>
2023-07-03 23:51:09 +0700

    init exercises

---
+++ exercise_1/target/debug/deps/basic_of_rust-941637dec24497a8.d
@@ -0,0 +1,8 @@
+C:\Users\Laffy\Desktop\rust-exercises\exercise_1\target\debug\deps\basic_of_rust-941637dec24497a8.rmeta: src\lib.rs src\conditions.rs src\strings.rs src\functions.rs
+
+C:\Users\Laffy\Desktop\rust-exercises\exercise_1\target\debug\deps\basic_of_rust-941637dec24497a8.d: src\lib.rs src\conditions.rs src\strings.rs src\functions.rs
+
+src\lib.rs:
+src\conditions.rs:
+src\strings.rs:
+src\functions.rs:
