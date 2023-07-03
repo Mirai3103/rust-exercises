e1f241d50458491c448c2e35e83cf534f8b33a22
Mirai3103 <huuhoag1412@gmail.com>
2023-07-04 00:28:59 +0700

    remove target folder

--- exercise_1/target/debug/deps/basic_of_rust-33116478449c7380.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Laffy\Desktop\rust-exercises\exercise_1\target\debug\deps\basic_of_rust-33116478449c7380.rmeta: src\lib.rs src\conditions.rs src\strings.rs src\functions.rs
-
-C:\Users\Laffy\Desktop\rust-exercises\exercise_1\target\debug\deps\basic_of_rust-33116478449c7380.d: src\lib.rs src\conditions.rs src\strings.rs src\functions.rs
-
-src\lib.rs:
-src\conditions.rs:
-src\strings.rs:
-src\functions.rs:
